aa2b152adccc4cad40b98ca1eae1dd86472a0604
StephonAutery <stephon@stephonautery.com>
2020-06-30 11:43:06 -0700

    deploy

---
+++ client/build/precache-manifest.ecf8623df34c2cdf1c0ecd3a61e9cea2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f3957a22290cfd446e6",
+    "url": "/static/js/main.288ed1bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4e5bc307c1bc0754708c",
+    "url": "/static/js/2.ae07c078.chunk.js"
+  },
+  {
+    "revision": "5aa78fe20d3aaf69161495f246d1d21a",
+    "url": "/index.html"
+  }
+];
